5f8e390ab05dad3b4836e352f9cfdcf4c5d7ce4d
ferszte <antoinefer@hotmail.com>
2020-04-29 18:25:34 +0200

    List containers functions

---
+++ src/main/docker/monitoring/container-list.go
@@ -0,0 +1,57 @@
+package monitoring
+
+import (
+	"context"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"test/src/main/jsonutils"
+)
+
+type containerInfo struct {
+	Name []string
+	Status string
+	Id string
+	Image string
+	Command string
+	Created int64
+	Ports []types.Port
+}
+
+func GetContainersInfo(allContainers bool) []byte{
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: allContainers})
+
+	var containersInfo []containerInfo
+	for _, container := range containers {
+		containersInfo = append(
+			containersInfo, containerInfo{
+			container.Names,
+			container.Status,
+			container.ID[:10],
+			container.Image,
+			container.Command,
+			container.Created,
+			container.Ports})
+	}
+
+	return jsonutils.FormatToJson(containersInfo)
+}
+
+func runningContainersId() []string{
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	runningContainers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: false})
+
+	var containersId []string
+	for _, container := range runningContainers {
+		containersId = append(containersId, container.ID[:10])
+	}
+
+	return containersId
+}
