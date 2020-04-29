26d7a7974db05f6651659479b32f9169937c642a
ferszte <antoinefer@hotmail.com>
2020-04-29 18:23:39 +0200

    Add http server

---
+++ src/main/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"test/src/main/configuration"
+	"test/src/main/endpoints"
+)
+
+func main() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	config := configuration.LoadConfig(pwd + "config.yml")
+	server := config.Server
+	router := endpoints.Routing()
+
+	log.Printf("Start http server at %v:%v", server.Host, server.Port)
+	log.Fatal(http.ListenAndServe(":"+server.Port, router))
+}
