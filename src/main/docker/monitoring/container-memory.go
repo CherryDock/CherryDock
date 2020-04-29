0b27f8f74d4e67b21b67b96b62931470ba5b4058
ferszte <antoinefer@hotmail.com>
2020-04-29 18:26:10 +0200

    Compute containers memory stats

---
+++ src/main/docker/monitoring/container-memory.go
@@ -0,0 +1,53 @@
+package monitoring
+
+import (
+	"context"
+	"github.com/docker/docker/client"
+	"io/ioutil"
+	jsonutils "test/src/main/jsonutils"
+)
+
+type MemoryStats struct {
+	MemoryUsage float64
+	Limit float64
+	UtilizationPercent float64
+}
+
+type ContainerMemory struct {
+	ContainerId string
+	Memory      MemoryStats
+}
+
+func getContainerMemory(containerId string) ContainerMemory {
+	cli, err := client.NewEnvClient()
+
+	if err != nil {
+		panic(err)
+	}
+	containerStats, err := cli.ContainerStats(context.Background(),containerId,false)
+	containerStatsBody, err := ioutil.ReadAll(containerStats.Body)
+
+	// Get data from json
+	dockerStats:= jsonutils.ParseJson(containerStatsBody)
+
+	// Extract memory field
+	memoryData := dockerStats["memory_stats"]
+	memoryStats := getMemoryStats(memoryData)
+
+	return 	ContainerMemory{containerId,memoryStats}
+}
+
+func getMemoryStats(memoryField interface{}) MemoryStats {
+	// Extract subfield to compute basics memory metrics
+	limit := memoryField.(map[string]interface{})["limit"].(float64)
+	usage := memoryField.(map[string]interface{})["usage"].(float64)
+	cache := memoryField.(map[string]interface{})["stats"].(map[string]interface{})["cache"].(float64)
+
+	// Compute memory usage in byte
+	memoryUsage := usage - cache
+	memoryUtilizationPercent := memoryUsage/limit * 100
+
+	memoryStats := MemoryStats{memoryUsage,limit,memoryUtilizationPercent}
+
+	return memoryStats
+}
