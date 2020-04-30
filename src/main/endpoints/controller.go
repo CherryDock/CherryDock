974040f3e9968fd2390a19f713aadec2562bbd41
ferszte <antoinefer@hotmail.com>
2020-04-30 15:44:02 +0200

    Add handle action route (single container action)

--- src/main/endpoints/controller.go
+++ src/main/endpoints/controller.go
@@ -4,24 +4,16 @@ import (
 	"github.com/Fszta/DockerMonitoring/src/main/docker/actions"
 	"github.com/Fszta/DockerMonitoring/src/main/docker/monitoring"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 )
 
 func Routing() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/api/memory-stats", getMemoryJson)
-	router.HandleFunc("/api/containers-info",getContainersInfoJson)
-	router.HandleFunc("/api/start-container/{id}", launchContainer)
-	router.HandleFunc("/api/start-container/{id}", launchContainer)
-	router.HandleFunc("/api/stop-container/{id}", stopContainer)
+	router.HandleFunc("/api/containers-info", getContainersInfoJson)
+	router.HandleFunc("/api/action/{action}/{id}", handleAction)
 
 	return router
-}
-
-func test(w http.ResponseWriter, r *http.Request){
-	vars := mux.Vars(r)
-	log.Print(vars["val"])
 }
 
 func getMemoryJson(w http.ResponseWriter, r *http.Request) {
@@ -32,18 +24,18 @@ func getMemoryJson(w http.ResponseWriter, r *http.Request) {
 
 func getContainersInfoJson(w http.ResponseWriter, r *http.Request) {
 	containersInfo := monitoring.GetContainersInfo(true)
-	w.Header().Set("content-type","application/json")
+	w.Header().Set("content-type", "application/json")
 	w.Write(containersInfo)
 }
 
-func launchContainer(w http.ResponseWriter, r *http.Request){
+func handleAction(w http.ResponseWriter, r *http.Request) {
 	parameters := mux.Vars(r)
-	actions.StartContainer(parameters["id"])
-	log.Printf("Start container %s",parameters["id"])
-}
+	action := parameters["action"]
+	id := parameters["id"]
 
-func stopContainer(w http.ResponseWriter, r *http.Request){
-	parameters := mux.Vars(r)
-	actions.StopContainer(parameters["id"])
-	log.Printf("Start container %s",parameters["id"])
+	// Handle single container action
+	actions.Handle(action, id)
+
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
 }
