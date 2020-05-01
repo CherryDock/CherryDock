41fedff09451307cd5ad396eca83e1eaaeab6000
ferszte <antoinefer@hotmail.com>
2020-05-01 03:24:02 +0200

    Fix route action parameters form

--- endpoints/controller.go
+++ endpoints/controller.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Fszta/DockerMonitoring/docker/actions"
 	"github.com/Fszta/DockerMonitoring/docker/monitoring"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
@@ -11,7 +12,7 @@ func Routing() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/api/memory-stats", getMemoryJson)
 	router.HandleFunc("/api/containers-info", getContainersInfoJson)
-	router.HandleFunc("/api/action/{action}/{id}", handleAction)
+	router.HandleFunc("/api/action/", handleAction)
 
 	return router
 }
@@ -29,11 +30,13 @@ func getContainersInfoJson(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleAction(w http.ResponseWriter, r *http.Request) {
-	parameters := mux.Vars(r)
+	/*parameters := mux.Vars(r)
 	action := parameters["action"]
-	id := parameters["id"]
-
+	id := parameters["id"]*/
+	action := r.FormValue("action")
+	id := r.FormValue("id")
 	// Handle single container action
+	log.Printf(action, id)
 	actions.Handle(action, id)
 
 	w.Header().Set("content-type", "application/json")
