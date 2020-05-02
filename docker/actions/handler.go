b625727e6fcce844576d18e5097b7b6e0f52ee02
ferszte <antoinefer@hotmail.com>
2020-05-02 19:37:57 +0200

    Add high order function action

--- docker/actions/handler.go
+++ docker/actions/handler.go
@@ -2,24 +2,47 @@ package actions
 
 import (
 	"context"
+	"github.com/Fszta/DockerMonitoring/docker/monitoring"
+	"github.com/Fszta/DockerMonitoring/jsonutils"
 	"github.com/docker/docker/client"
+	"log"
 )
 
-func Handle(action string, containerId string) {
+type action func(ctx context.Context, cli *client.Client, containerId string) bool
+
+type ActionState struct {
+	ContainerId string
+	Succeed bool
+}
+
+func ActionSingleContainer(singleAction action, containerdId string) bool {
 	ctx := context.Background()
 	cli, err := client.NewEnvClient()
 
 	if err != nil {
-		panic(err)
+		log.Println("Fail to create docker client")
 	}
 
-	switch action {
-	case "start":
-		StartContainer(ctx, cli, containerId)
-	case "stop":
-		StopContainer(ctx, cli, containerId)
-	default:
-		panic("Unknow Action")
+	succeed := singleAction(ctx, cli, containerdId)
+
+	return succeed
+}
+
+func ActionOnAllContainer(singleAction action) []byte {
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+
+	if err != nil {
+		log.Println("Fail to create docker client")
 	}
 
+	var states []ActionState
+
+	allContainers := monitoring.ContainersId(true)
+	for _ , id := range allContainers {
+		succeed := singleAction(ctx, cli, id)
+		states  = append(states,ActionState{id,succeed})
+	}
+
+	return jsonutils.FormatToJson(states)
 }
