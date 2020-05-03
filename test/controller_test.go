01a4f6e817e0ffcbaa3686d04c3f40ef923d3c3d
ferszte <antoinefer@hotmail.com>
2020-05-03 17:37:45 +0200

    Add test restart single container

--- test/controller_test.go
+++ test/controller_test.go
@@ -68,3 +68,30 @@ func TestStop(t *testing.T) {
 		res.Body.Close()
 	}
 }
+
+func TestRestart(t *testing.T) {
+	// Test restart single container
+	var url = "http://0.0.0.0:8001/api/action/stop?id="
+	stopWithEmptyId := SingleActionTest{"GET", url, "", http.StatusBadRequest}
+	stopWithUnknowId := SingleActionTest{"GET", url, "fakeId", http.StatusNotFound}
+
+	testActions := [2]SingleActionTest{stopWithEmptyId, stopWithUnknowId}
+
+	for _, test := range testActions {
+		req, err := http.NewRequest(test.Method, test.Url+test.Id, nil)
+
+		if err != nil {
+			t.Fatalf("could not create request %s", err)
+		}
+		recorder := httptest.NewRecorder()
+		endpoints.RestartSingle(recorder, req)
+
+		res := recorder.Result()
+
+		if res.StatusCode != test.ExpectedStatus {
+			t.Fatalf("Expected status %v", test.ExpectedStatus)
+		}
+
+		res.Body.Close()
+	}
+}
