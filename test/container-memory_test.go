f9643fd94657d055d049bb7ed11b5aab5d737648
ferszte <antoinefer@hotmail.com>
2020-05-03 15:41:06 +0200

    Add compute stats test

--- test/container-memory_test.go
+++ test/container-memory_test.go
@@ -1,7 +1,28 @@
 package test
 
 import "testing"
+import "github.com/Fszta/DockerMonitoring/docker/monitoring"
 
-func testMemory(t *testing.T){
-	t.Fatalf("Fail")
+func TestComputeStats(t *testing.T) {
+
+	memoryStats := monitoring.ComputeStats(4.0, 2.0, 1.0)
+	expected := monitoring.MemoryStats{
+		1,
+		4,
+		25.0,
+	}
+	if memoryStats != expected {
+		t.Errorf("Memory stats should be %v", expected)
+	}
+
+	memoryStats = monitoring.ComputeStats(1.0, 0, 0)
+	expected = monitoring.MemoryStats{
+		0,
+		1,
+		0,
+	}
+
+	if memoryStats != expected {
+		t.Errorf("Memory stats should be %v", expected)
+	}
 }
