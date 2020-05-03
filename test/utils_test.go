9c820ab5bd848b45962572679af7defca377e47b
ferszte <antoinefer@hotmail.com>
2020-05-03 18:52:53 +0200

    Json utils test

---
+++ test/utils_test.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	"github.com/Fszta/DockerMonitoring/jsonutils"
+	"testing"
+)
+
+func TestFormatToJson(t *testing.T) {
+	type TestStruct struct {
+		Val1 float64
+		Val2 string
+	}
+
+	test := TestStruct{1.0, "test"}
+
+	formated := jsonutils.FormatToJson(test)
+	var expected = `{"Val1":1,"Val2":"test"}`
+
+	if string(formated) != expected {
+		t.Fatalf("Error, output should be %s", expected)
+	}
+}
+
+func TestParseJson(t *testing.T) {
+	type TestStruct struct {
+		Val1 float64
+		Val2 string
+	}
+
+	test := TestStruct{1.0, "test"}
+
+	formated := jsonutils.FormatToJson(test)
+
+	parsed := jsonutils.ParseJson(formated)
+
+	if parsed["Val1"] != 1.0 || parsed["Val2"] != "test" {
+		t.Fatal("Fail to parse to json")
+	}
+
+}
