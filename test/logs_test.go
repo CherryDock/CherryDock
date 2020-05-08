66158dff6cc53a68247bcaace4f6d5abaf460c2c
ferszte <antoinefer@hotmail.com>
2020-05-08 18:38:18 +0200

    Add test retrieve logs

---
+++ test/logs_test.go
@@ -0,0 +1,25 @@
+package test
+
+import (
+	"github.com/Fszta/DockerMonitoring/docker/monitoring"
+	"testing"
+)
+
+type TestLogs struct {
+	ContainerId         string
+	ExpectedLogsContent []byte
+	ExpectedStatus      bool
+}
+
+func TestRetrieveLogs(t *testing.T) {
+	var expectedLogs []byte
+	expectedStatus := false
+
+	testLogsBadId := TestLogs{"xxxxxx", expectedLogs, expectedStatus}
+
+	logs, succeed := monitoring.RetrieveLogs(testLogsBadId.ContainerId)
+
+	if len(logs) != 0 && succeed != expectedStatus {
+		t.Fatalf("Logs should be empty & succeed equal to false")
+	}
+}
