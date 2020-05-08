636ec23a09a11978b687ddb2668262aa8b63a4dc
ferszte <antoinefer@hotmail.com>
2020-05-08 18:07:34 +0200

    Handle case retrieve logs fail, add bool to return statement

--- docker/monitoring/logs.go
+++ docker/monitoring/logs.go
@@ -8,19 +8,21 @@ import (
 	"log"
 )
 
-func RetrieveLogs(containerId string) []byte {
+func RetrieveLogs(containerId string) ([]byte, bool) {
 	cli, err := client.NewEnvClient()
 	if err != nil {
 		log.Println(err)
 	}
+	var logs []byte
 
 	options := types.ContainerLogsOptions{ShowStdout: true}
 	out, err := cli.ContainerLogs(context.Background(), containerId, options)
+
 	if err != nil {
 		log.Printf("Fail to get logs for container %s", containerId)
+		return logs, false
 	} else {
-
+		logs, err = ioutil.ReadAll(out)
+		return logs, true
 	}
-	logs, err := ioutil.ReadAll(out)
-	return logs
 }
