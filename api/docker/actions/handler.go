8bebff8e74c88bc0e3ea1f7f84c3bed3da5cf4a0
ferszte <antoinefer@hotmail.com>
2020-05-16 02:25:37 +0200

    #27 Issue - update action on single container, add action succeed boolean

--- api/docker/actions/handler.go
+++ api/docker/actions/handler.go
@@ -8,14 +8,7 @@ import (
 	"log"
 )
 
-type action func(ctx context.Context, cli *client.Client, containerId string) bool
-
-type ActionState struct {
-	ContainerId string
-	Succeed     bool
-}
-
-func ActionSingleContainer(singleAction action, containerdId string) bool {
+func ActionSingleContainer(singleAction action, containerId string) bool {
 	ctx := context.Background()
 	cli, err := client.NewEnvClient()
 
@@ -23,7 +16,14 @@ func ActionSingleContainer(singleAction action, containerdId string) bool {
 		log.Println("Fail to create docker client")
 	}
 
-	succeed := singleAction(ctx, cli, containerdId)
+	var succeed bool
+	err = singleAction(ctx, cli, containerId)
+
+	if err != nil {
+		succeed = false
+	} else {
+		succeed = true
+	}
 
 	return succeed
 }
@@ -36,12 +36,18 @@ func ActionOnAllContainer(singleAction action, all bool) []byte {
 		log.Println("Fail to create docker client")
 	}
 
-	var states []ActionState
+	var states []actionState
+	var succeed bool
 
 	allContainers := monitoring.ContainersId(all)
 	for _, id := range allContainers {
-		succeed := singleAction(ctx, cli, id)
-		states = append(states, ActionState{id, succeed})
+		err = singleAction(ctx, cli, id)
+		if err != nil {
+			succeed = false
+		} else {
+			succeed = true
+		}
+		states = append(states, actionState{id, succeed})
 	}
 
 	return jsonutils.FormatToJson(states)
