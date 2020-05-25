15d82564a68596a78421dced29c2e4382d0cb5fc
ferszte <antoinefer@hotmail.com>
2020-05-25 18:31:37 +0200

    move to http package - add config as parameter

--- api/http/server.go
+++ api/http/server.go
@@ -1,22 +1,15 @@
-package main
+package http
 
 import (
 	"github.com/CherryDock/CherryDock/api/configuration"
 	"github.com/CherryDock/CherryDock/api/endpoints"
 	"log"
 	"net/http"
-	"os"
 )
 
-func main() {
-	pwd, err := os.Getwd()
-	if err != nil {
-		log.Println(err)
-	}
-	config := configuration.LoadConfig(pwd + "/config.yml")
+func StartServer(config *configuration.Configuration) {
 	server := config.Server
 	router := endpoints.Routing()
-
 	log.Printf("Start http server at %v:%v", server.Host, server.Port)
 	log.Fatal(http.ListenAndServe(":"+server.Port, router))
 }
